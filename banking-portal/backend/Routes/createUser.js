76a311202bac098ab06c54e1150fa3973f4ae80b
Akshavya2509 <Akshavya2509@gmail.com>
2024-06-12 19:30:10 +0530

    Added validation

--- banking-portal/backend/Routes/createUser.js
+++ banking-portal/backend/Routes/createUser.js
@@ -1,27 +1,40 @@
 const express = require('express')
 const router = express.Router();
 const users = require('../models/Users');
+const { body, validationResult } = require('express-validator')
 
-router.post("/createUser", async (req, res) => {
-    try{
-        await users.create({
-            name: "Rajesh koothrapali",
-            age: 25,
-            address: {
-                locality: 'cenral perk',
-                house_no: 124,
-                street: '15-A',
-                city: 'Massachusetts',
-                postcode: '01094',
-            },
-            email_id: "rajkooth123@hotmail.com"
-        })
-        res.json({success: true});
-    }
-    catch(err){
-        console.log(err);
-        res.json({success: false});
-    }
-});
+router.post(
+    "/createUser",
+    [body('email_id', "Incorrect email-id").isEmail()],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        try{
+            await users.create({
+                name: req.body.name,
+                age: req.body.age,
+                address: {
+                    locality: req.body.address.locality,
+                    sub_area: req.body.address.sub_area,
+                    house_no: req.body.address.house_no,
+                    street: req.body.address.street,
+                    sec_street: req.body.address.sec_street,
+                    city: req.body.address.city,
+                    postcode: req.body.address.postcode
+                },
+                email_id: req.body.email_id
+            });
+            res.json({success: true});
+        }
+        catch(err){
+            console.log(err);
+            res.json({success: false});
+        }
+
+    });
 
 module.exports = router;
