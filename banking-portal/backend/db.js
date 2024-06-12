18eef91ea642934021b32c80e828cd35506750e4
Akshavya2509 <Akshavya2509@gmail.com>
2024-06-12 18:33:51 +0530

    Basic Schema created

--- banking-portal/backend/db.js
+++ banking-portal/backend/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb+srv://akshavya2509:Ly2mUsR9zBlChy2y@cluster0.olzvwz6.mongodb.net/';
+const mongoURL = 'mongodb+srv://akshavya2509:Ly2mUsR9zBlChy2y@cluster0.olzvwz6.mongodb.net/banking-portal';
 
 const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURL);
         console.log("Database Connected Successfully");
+        const fetched_data = await mongoose.connection.db.collection("users")
+        fetched_data.find({}).toArray(function(err, data){
+            if(err) console.log(err);
+            else console.log(data)
+        })
     } catch (error) {
         console.error("Database connection error:", error);
         process.exit(1); // Exit process with failure
---
+++ banking-portal/backend/models/Users.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    address: {
+        locality:{
+            type: String,
+            required: true
+        },
+        sub_area:{
+            type: String,
+            required: false
+        },
+        house_no:{
+            type: Number,
+            required: true
+        },
+        street:{
+            type: String,
+            required: true
+        },sec_street:{
+            type: String,
+            required: false
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        postcode: {
+            type: Number,
+            required: true
+        },
+
+        required: true
+    },
+    email_id:{
+        type: String,
+        required: true
+    }
+})
